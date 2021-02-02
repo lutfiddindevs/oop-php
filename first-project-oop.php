cb584965cd10a78c071a040b8938e3754b5fe2d8
[Lutfiddin Shamsidinov] <[shamsidinovlutfiddin@gmail.com]>
2021-02-02 21:17:10 +0500

    the first project using oop calculator inputs

---
+++ first-project-oop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculator using OOP</title>
+</head>
+<body>
+<form action="functions.php" method="post">
+	<h4>Calculator using object oriented way of programming</h4>
+	<input type="number" name="number1" placeholder="Enter number 1">
+	<select name="oper">
+		<option value="none">None</option>
+		<option value="add">Add</option>
+		<option value="substract">Substract</option>
+		<option value="multiply">Multiply</option>
+		<option value="divide">Divide</option>
+	</select>
+	<input type="number" name="number2" placeholder="Enter number 2">
+	<button name="submit" type="submit">Calculate</button>
+</form>
+</body>
+</html>
